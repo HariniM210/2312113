dbe18b18ba8a6ce93070f0422bce21c369844b5b
HariniM210 <harinimanikandan316@gmail.com>
2026-07-02 11:28:54 +0530

    frontend function added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,63 @@ function App(){
   useEffect(()=>{
     getNotifications()},[])
     async function getNotification(){
+      try{
       const response=await axios.get("http://localhost/3000/notifications")
+      console.log(response.data);
+      setNotification(response.data);
+      }
+      catch(error){
+        console.log(error);
+      }
+    }
+    async function addNotification(){
+      const newNotification={
+        id,
+        Type,
+        Message,
+        Timestamp
+      }
+      try{
+        await axios.post("http://localhost:3000/notifications",newNotification);
+        getNotification();
+        clearform();
+      }
+      catch(error){
+        console.log(error);
+      }
+    }
+    async function deletenotification(notificationsid){
+      try{
+        await axios.delete(`http://localhost:3000/notifications/$(notificationsid)`)
+        getNotifications();
+        clearform();
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    async function updateNotification(){
+      try{
+        getNotifications();
+        setEditId(null);
+        clearforms();
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    async function editNotification(){
+      setId(notifications.id);
+      setType(notifications.Type);
+      setMessage(notifications.Message);
+      setTimestamp(notifications.Timestamp);
+    }
+    function clearform(){
+      setId("");
+      setType("");
+      setMessage("");
+      setTimestamp("");
+      setEditId("");
     }
 
 }
